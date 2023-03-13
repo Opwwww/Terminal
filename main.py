79ca99fa7ee51b4e15509fd2886c5a43a76af3e8
Opwwww <107410539+Opwwww@users.noreply.github.com>
2023-03-13 18:14:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -161,8 +161,6 @@ while True:
    webbrowser.open(web2)
  elif cmd == "tree":
   s("tree")
- #afahrhrahar@gmail.com
-#baxpggudntwjddfs
  elif cmd == "reload":
   s("python main.py")
  elif cmd == "explorer":
